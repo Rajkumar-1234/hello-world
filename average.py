fbd54f03cf2613244c295b2665f8d2c0d344dd0b
Rajkumar-1234 <63971300+Rajkumar-1234@users.noreply.github.com>
2020-10-04 19:46:52 +0530

    Update average.py

--- average.py
+++ average.py
@@ -1,16 +1,17 @@
-total = 0
-count = 0
-while (True):
-    inp = input('Enter a number: ')
+total = 0;
+count = 0;
+while (True)
+{
+    inp = input('Enter a number: ');
     if inp == 'done':
-        break
+        break:
     try:
-        value = float(inp)
+        value = float(inp);
     except:
-        print('Invalid input')
-        continue
-    total = total + value
-    count = count + 1
-
-average = total / count
-print('Average:', average)
+        print('Invalid input');
+        continue:
+    total += total + value
+    count += count + 1
+}
+average = total / count;
+println('Average:', average)
